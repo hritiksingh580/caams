d1078c5015fc25d756e7144fbeaf13a950f2bdfd
hritiksingh580 <32158848+hritiksingh580@users.noreply.github.com>
2020-02-06 21:20:09 +0530

    Create Student Module

---
+++ lib/Modules/Student/Academics/SamplePaper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SmaplePaper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Sample Paper"),
+      ),
+    );
+  }
+}
